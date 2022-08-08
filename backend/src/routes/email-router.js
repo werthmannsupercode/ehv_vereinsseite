52d694bd0fd1fe474d37d9ef8bda8fcc185bcfd2
werthmannsupercode <werthmannsupercode@gmail.com>
2022-08-08 15:30:06 +0200

    kontaktform css

--- backend/src/routes/email-router.js
+++ backend/src/routes/email-router.js
@@ -15,7 +15,8 @@ emailRouter.post("/neueanfrage", (req, res) => {
         email: req.body.email,
         name: req.body.name,
         datumDerAnfrage: Date.now(),
-        text: req.body.text
+        text: req.body.text,
+        phone: req.body.phone
     }
 
     sendMail({
@@ -41,7 +42,11 @@ emailRouter.post("/neueanfrage", (req, res) => {
 
         Nachricht: 
 
-        ${Anfrage.text}`
+        ${Anfrage.text}
+        
+        Telefonnummer: 
+        
+        ${Anfrage.phone}`
     })
 
     neueAnfrage(Anfrage)
@@ -51,97 +56,6 @@ emailRouter.post("/neueanfrage", (req, res) => {
             res.status(500).json({ error: "Failed to add request to database." })
         })
 })
-// const usersArray = [
-//     {
-//         email: "ahmed@super-code.de",
-//         name: "Ahmed Nasr",
-//         joinedAt: 1658222679370,
-//         verifiedEmail: true
-//     }
-// ]
-
-// app.get("/users", (_, res) => {
-//     const verifiedUsers = usersArray.filter(u => u.verifiedEmail === true)
-//     res.json(verifiedUsers)
-// })
-
-// app.post("/users", (req, res) => {
-
-//     sendMail({
-//         to: newUser.email,
-//         subject: "Please verify your email...",
-//         message: `
-//         Hello ${newUser.name},
-
-//         thank you for joining Keller-Kinder!
-
-//         Please use this six-digit verification code to verify your email.
-
-//         ${newUser.sixDigitCode}
-
-//         After that you will be shown on the member list.
-
-//         Looking forward to do kellering with you.
-
-//         Yours,
-//         Keller-Kinder CEO 
-//         `
-//     }).then(wasSent => {
-//         if(wasSent) {
-//             usersArray.push(newUser)
-//             res.status(200).json({
-//                 email: newUser.email,
-//                 name: newUser.name,
-//                 joinedAt: newUser.joinedAt,
-//                 verifiedEmail: newUser.verifiedEmail
-//             })
-//         } else {
-//             res.status(500).json({ error: "Internal Server Error, please try joining again later."})
-//         }
-//     })
-// })
-
-// app.put("/users/verify", (req, res) => {
-//     const userEmail = req.body.email
-//     const sixDigitCodeClaim = req.body.sixDigitCode
-
-//     const user = usersArray.find(u => u.email === userEmail)
-//     if(!user) {
-//         return res.status(400).json({ error: "Bad Request. User not found." })
-//     }
-
-//     const codeWasCorrect = user.sixDigitCode === sixDigitCodeClaim
-//     if(!codeWasCorrect) {
-//         return res.status(400).json({ error: "Bad Request. Code incorrect." })
-//     }
-
-//     // VERIFY USER EMAIL, everything is fine...
-//     user.verifiedEmail = true;
-//     res.status(200).json({
-//         email: user.email,
-//         name: user.name,
-//         joinedAt: user.joinedAt,
-//         verifiedEmail: user.verifiedEmail
-//     })
-// })
-
-
-
-// sendMail({ 
-//     to: "ahmed@super-code.de",
-//     subject: "Mr. Fredoni, we need you! Part 5",
-//     message: `
-//     Hi Mr. Fredoni,
-
-//     we urgently need a full stack dev.
-//     Are you available?
-
-//     If so please call me 0129303030.
-
-//     Yours Truly,
-//     Ahmed from Super Coders Pro
-//     `
-// })
 
 module.exports = {
     emailRouter
--- frontend/src/pages/Kontakt.js
+++ frontend/src/pages/Kontakt.js
@@ -2,14 +2,16 @@ import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 import Footer from "../components/Footer";
 import Map from "../components/Map/Map";
-import { useState, useEffect } from "react";
-import { apiBaseUrl } from "./../api"
+import { useState } from "react";
+import { apiBaseUrl } from "./../api";
+import "./../styles/Kontakt.css"
 
 const Kontakt = () => {
 
     const [name, setName] = useState("")
     const [email, setEmail] = useState("")
     const [text, setText] = useState("")
+    const [phone, setPhone] = useState("")
 
     const sendNewRequest = () => {
 
@@ -21,7 +23,8 @@ const Kontakt = () => {
             body: JSON.stringify({
                 name,
                 email,
-                text
+                text,
+                phone
             })
         }).then(res => res.json(), alert("Vielen Dank für deine Anfrage. Wir haben dir eine Eingangsbestätigung an deine Emailaddresse geschickt.")
         )
@@ -30,6 +33,30 @@ const Kontakt = () => {
 
     return (<div>
         <Navbar />
+        <div>
+            <h1 className="h1Kontakt">Hallo, schön, dass du da bist. Wir freuen uns von dir zu hören. Schicke uns einfach eine Nachricht.</h1>
+            <form>
+                <div className="firstBlock">
+                    <div>
+                        <label htmlFor="name" className="labelKontakt">NAME</label>
+                        <input type="text" name="name" id="name" placeholder="Gib hier deinen Namen ein." value={name} onChange={(e) => setName(e.target.value)} />
+                    </div>
+                    <div>
+                        <label htmlFor="email" className="labelKontakt">EMAIL ADDRESSE</label>
+                        <input type="email" id="email" name="email" placeholder="Gib hier deine Mailaddresse ein." value={email} onChange={(e) => setEmail(e.target.value)} />
+                    </div>
+                </div>
+                <div className="secondBlock">
+                    <label htmlFor="phone" className="labelKontakt">TELEFONNUMMER</label>
+                    <input type="text" name="phone" id="phone" placeholder="Gib hier deine Telefonnummer ein." value={phone} onChange={(e) => setPhone(e.target.value)} />
+                </div>
+                <div className="thirdBlock">
+                    <label htmlFor="nachricht" className="labelKontakt">NACHRICHT</label>
+                    <textarea name="nachricht" id="nachricht" cols="30" rows="10" value={text} onChange={(e) => setText(e.target.value)} placeholder="Deine Nachricht an uns">Hier kannst du deine Nachricht an uns eingeben.</textarea>
+                </div>
+                <input type="submit" value="ABSENDEN" onClick={sendNewRequest} className="submitKontakt" />
+            </form>
+        </div>
         <div className="kontakt">
             <h1>Kontakt</h1>
             <div>
@@ -55,15 +82,6 @@ const Kontakt = () => {
                     </div>
                 </div>
             </div>
-            <div>
-                <form>
-                    <input type="text" name="name" id="name" placeholder="Dein Name" value={name} onChange={(e) => setName(e.target.value)} />
-                    <input type="email" id="email" name="email" placeholder="Deine Mailaddresse" value={email} onChange={(e) => setEmail(e.target.value)} />
-                    <textarea name="nachricht" id="nachricht" cols="30" rows="10" value={text} onChange={(e) => setText(e.target.value)}>Hier kannst du deine Nachricht an uns eingeben.</textarea>
-                    <input type="submit" value="Absenden" onClick={sendNewRequest} />
-                </form>
-            </div>
-
         </div>
         <Map />
         <Sidebar />
