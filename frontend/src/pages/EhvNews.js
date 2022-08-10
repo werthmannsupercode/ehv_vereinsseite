6e882e740a71cdb15f261d5b8f875514ecc8bbd0
werthmannsupercode <werthmannsupercode@gmail.com>
2022-08-10 08:57:47 +0200

    started newspaper

--- frontend/src/pages/EhvNews.js
+++ frontend/src/pages/EhvNews.js
@@ -2,10 +2,17 @@ import Navbar from "../components/Navigation/Navbar";
 import Sidebar from "../components/Sidebar";
 import Footer from "../components/Footer";
 
+import "./../styles/EhvNewsStyles.css";
+
 const EhvNews = () => {
-    return (<div>
+    return (<div className="news">
         <Navbar />
-        <h1>EHV News</h1>
+        <h1 className="newsHaedline">EIGENHEIMER AKTUELL</h1>
+        <article className="newsSubtitle">
+            <h2>2022</h2>
+            <h2>NACHRICHTEN UND BERICHTE VON UNSEREN VERANSTALTUNGEN</h2>
+            <h2>FIRST EDITION</h2>
+        </article>
         <Sidebar />
         <Footer />
     </div>);
