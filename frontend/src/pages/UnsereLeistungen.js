f81845c616cc200121e0835c045a0e2ed919182a
werthmannsupercode <werthmannsupercode@gmail.com>
2022-08-11 14:12:31 +0200

    unsere Leistungen done

--- frontend/src/pages/UnsereLeistungen.js
+++ frontend/src/pages/UnsereLeistungen.js
@@ -2,12 +2,28 @@ import Navbar from "../components/Navigation/Navbar";
 import Sidebar from "../components/Sidebar";
 import Footer from "../components/Footer";
 import "../styles/UnsereLeistungenStyles.css";
+import Blumengirlande from "../images/Blumengirlande.png";
 
 const UnsereLeistungen = () => {
     return (<div>
         <Navbar />
         <div className="unsereleistungen">
             <h1>Unsere Leistungen</h1>
+            <div>
+                <p>Über den Eigenheimerverband Bayern e.V. gibt es u.a. folgende Leistungen für unsere Mitglieder:</p>
+                <ul>
+                    <li>Bauherren-Haftpflichtversicherung</li>
+                    <li>Haus- und Grundstückshaftpflichtversicherung</li>
+                    <li>Günstige, maßgeschneiderte Zusatzversicherungen</li>
+                    <li>Beratung für Bauherren</li>
+                    <li>Gartenfachliche Beratung</li>
+                    <li>Rechtsberatung</li>
+                    <li>Zeitschrift „Eigenheimer Magazin“</li>
+                    <li>Interessenvertretung der Eigenheimbesitzer</li>
+                    <li>Einkaufsvergünstigungen</li>
+                </ul>
+            </div>
+            <img src={Blumengirlande} alt="Blumengirlande" />
         </div>
         <Sidebar />
         <Footer />
